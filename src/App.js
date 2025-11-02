30c8f27fcec2341d6163c380b5e8b68839b27a20
divyam189 <Divyamaggarwal189@gmail.com>
2025-11-02 22:50:57 +0530

    Initial portfolio commit

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ function App() {
         </motion.p>
         <div className="flex gap-5 flex-wrap justify-center">
        <a
-  href="/Divyam-Aggarwal-Resume.pdf.pdf"
+  href="/resumev2.pdf"
   target="_blank"
   rel="noreferrer"
   className="bg-black dark:bg-white dark:text-black text-white hover:opacity-80 px-8 py-3 rounded-lg font-semibold shadow-md transition-all duration-300"
